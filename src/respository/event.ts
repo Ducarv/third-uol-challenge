42e4c0ec56139a4acfc57bb4705c62dc330400f5
Eduardo Carvalho <educarvalho009@gmail.com>
2023-12-26 17:27:17 -0400

    feat: event prisma implementation

--- src/respository/event.ts
+++ src/respository/event.ts
@@ -3,7 +3,7 @@ import { IEvent } from "../domain/entities/Event";
 export interface EventRepository {
     create: (data: IEvent) => Promise<IEvent>;
     getEventByQuery: (dayOfWeek?: string, desc?: string) => Promise<IEvent[]>;
-    deleteEventByDayOfWeek: (day: string) => Promise<IEvent[]>;
+    deleteEventsByDayOfWeek: (day: string) => Promise<IEvent[] | string>;
     getEventById: (id: string) => Promise<IEvent>;
     deleteEventById: (id: string) => Promise<string | void>
 }
---
+++ src/respository/implementation/event.ts
@@ -0,0 +1,58 @@
+import { IEvent } from "../../domain/entities/Event";
+import { prisma } from "../../infra/database";
+import { EventRepository } from "../event";
+
+export class EventPrismaRepository implements EventRepository {
+    constructor() {};
+
+    async create(data: IEvent) {
+        const newEvent = await prisma.event.create({
+            data: data
+        })
+
+        return newEvent as IEvent;
+    }
+
+    async getEventByQuery(dayOfWeek?: string, desc?: string) {
+        const events = await prisma.event.findRaw({
+            filter: {
+               $or: [
+                { dayOfWeek },
+                { desc }
+               ]
+            }
+        })
+
+        return events as unknown as IEvent[]
+    }
+
+    async deleteEventsByDayOfWeek(dayOfWeek: string) {
+        const deletedEvents = await prisma.event.deleteMany({
+            where: {
+                dayOfWeek
+            }
+        })
+
+        return `${dayOfWeek} events deleted`
+    }
+
+    async getEventById(id: string) {
+        const target = await prisma.event.findUnique({
+            where: {
+                id
+            }
+        })
+
+        return target as IEvent;
+    }
+
+    async deleteEventById(id: string) {
+        const deletedEvent = await prisma.event.delete({
+            where: {
+                id
+            }
+        })
+
+        return `Event with id: ${id} deleted.`
+    }
+}
