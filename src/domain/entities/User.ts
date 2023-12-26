383f69e2bb5e92a37a7ff2fda4499f77a249b9a8
Eduardo Carvalho <educarvalho009@gmail.com>
2023-12-26 15:34:21 -0400

    refactor: user props

---
+++ src/domain/entities/User.ts
@@ -0,0 +1,14 @@
+import { IEvent } from "./Event"
+
+export interface IUser {
+    id?: string
+    firstName: string
+    lastName: string
+    birthDate: Date
+    city: string
+    country: string
+    email: string
+    password: string
+    confirmPassword: string
+    events?: IEvent[]
+}
