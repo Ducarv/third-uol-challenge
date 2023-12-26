954e78bccd6bf4fdb80e94e894dfc75815656f9f
Eduardo Carvalho <educarvalho009@gmail.com>
2023-12-26 16:28:15 -0400

    config/add: cors and JWT

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors"
 import "dotenv/config"
 
 const app = express();
@@ -6,6 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 
 app.listen(PORT, () => {
     console.log('Server running!')
