e854d7c235297467c2c584ab195b943bfcef63a6
Eduardo Carvalho <educarvalho009@gmail.com>
2023-12-27 14:57:42 -0400

    feat: auth middleware

---
+++ src/providers/middlewares/auth.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from "express";
+import * as jwt from "jsonwebtoken"
+import "dotenv/config"
+
+export function authToken(request: Request, response: Response, next: NextFunction) {
+    const token = request.headers.authorization;
+
+    if(!token) {
+        return response.status(401).json({ message: "Token not provided"})
+    }
+
+    const secret = process.env.SECRET_KEY as jwt.Secret;
+
+    jwt.verify(token, secret, (err, decoded) => {
+        if(err) {
+            return response.status(401).json({ message: " Token invalid" });
+        }
+
+        request.body.user = decoded;
+
+        next();
+    })
+}
