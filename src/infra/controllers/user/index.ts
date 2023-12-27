5df4688e32f881e03b5de9c9bd69ae39f544c3c3
Eduardo Carvalho <educarvalho009@gmail.com>
2023-12-27 14:39:46 -0400

    feat: signIn routes

--- src/infra/controllers/user/index.ts
+++ src/infra/controllers/user/index.ts
@@ -1,9 +1,14 @@
+import { SignInUserUseCase } from "../../../domain/useCases/user/signIn";
 import { SignUpUserUseCase } from "../../../domain/useCases/user/signUp";
 import { UserPrismaRepository } from "../../../respository/implementation/user";
 import { SignUpUserController } from "./signUp";
+import { SingInUserController } from './signIn';
 
 const prismaRepository = new UserPrismaRepository();
 const signUpUserUseCase = new SignUpUserUseCase(prismaRepository);
 const signUpUserController = new SignUpUserController(signUpUserUseCase);
 
-export { signUpUserController };
+const signInUserUseCase = new SignInUserUseCase(prismaRepository);
+const signInUserController = new SingInUserController(signInUserUseCase);
+
+export { signUpUserController, signInUserController };
--- src/infra/routes/index.ts
+++ src/infra/routes/index.ts
@@ -1,10 +1,14 @@
 import { Request, Response, Router } from "express";
-import { signUpUserController } from "../controllers/user";
+import { signInUserController, signUpUserController } from "../controllers/user";
 
 const router = Router();
 
 router.post("/users/sign-up", async (request: Request, response: Response) => {
     await signUpUserController.handle(request, response);
+})
+
+router.post("/users/sign-in", async (request: Request, response: Response) => {
+    await signInUserController.handle(request, response);
 })
 
 export { router }
