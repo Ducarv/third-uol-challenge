1a7e84a897ef5bcd762de93d19b5c673776af4a2
Eduardo Carvalho <educarvalho009@gmail.com>
2023-12-27 13:15:24 -0400

    test: signIn

---
+++ src/domain/useCases/user/__tests__/signIn.spec.ts
@@ -0,0 +1,60 @@
+import { SignInUserUseCase } from "../signIn"; // Nome do arquivo do caso de uso SignIn
+import { IUser } from "../../../entities/User";
+import { SignInError } from "../../../../providers/errors";
+import { UserRepository } from "../../../../respository/user";
+
+describe("SignIn.ts", () => {
+  let mockUser: IUser = {
+    firstName: "User",
+    lastName: "Mock",
+    birthDate: new Date(),
+    city: "Toronto",
+    country: "Canada",
+    email: "user@mail.com",
+    password: "123",
+    confirmPassword: "123",
+    events: [],
+  };
+
+  let mockRepository: Pick<UserRepository, "signIn"> = {
+    signIn: async (email: string, password: string) => {
+      if (email === mockUser.email && password === mockUser.password) {
+        const { password, confirmPassword, ...mockResponse } = mockUser;
+        return mockResponse as Partial<IUser>;
+      } else {
+        return {} as Partial<IUser>;
+      }
+    },
+  };
+
+  let signIn: SignInUserUseCase;
+
+  beforeEach(() => {
+    signIn = new SignInUserUseCase(mockRepository as UserRepository);
+  });
+
+  it("should sign a user in successfully", async () => {
+    const { password, confirmPassword, ...expectedUser } = mockUser as Partial<IUser>;
+
+    const sut = await signIn.execute(mockUser.email, mockUser.password);
+    expect(sut).toEqual(expect.objectContaining(expectedUser));
+  });
+
+  it("should handle incorrect email or password correctly", async () => {
+    const sut = await signIn.execute('wrongemail@mail.com', 'wrongpassword');
+    expect(sut).toEqual({});
+  });
+
+  it("should handle SignInError correctly", async () => {
+    const error = new SignInError("Error to sign user in.");
+    jest.spyOn(mockRepository, "signIn").mockRejectedValueOnce(error);
+
+    try {
+      await signIn.execute(mockUser.email, mockUser.password);
+      fail("Should throw SignInError");
+    } catch (caughtError: unknown) {
+      expect(caughtError).toBeInstanceOf(SignInError);
+      expect(caughtError).toEqual(error);
+    }
+  });
+});
