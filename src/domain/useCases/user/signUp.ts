3900ffdf827272a739318c37b7ef606a98f18557
Eduardo Carvalho <educarvalho009@gmail.com>
2023-12-27 09:13:56 -0400

    refactor: sigUp user case, hash password

--- src/domain/useCases/user/signUp.ts
+++ src/domain/useCases/user/signUp.ts
@@ -1,12 +1,35 @@
+import { SignUpError } from "../../../providers/errors";
 import { UserRepository } from "../../../respository/user";
 import { IUser } from "../../entities/User";
+import crypto from "node:crypto";
 
 export class SignUpUserUseCase {
-    constructor(private respotory: UserRepository) {}
+  constructor(private respotory: UserRepository) {}
 
-    async execute(data: IUser) {
-        const newUser = await this.respotory.signUp(data)
-
-        return newUser;
+  async execute(data: IUser) {
+    try {
+        const newUser = await this.respotory.signUp(data);
+    
+        const hashPassword = crypto
+          .createHash("sha256")
+          .update(data.password)
+          .digest("hex");
+        const hashPasswordConfirm = crypto
+          .createHash("sha256")
+          .update(data.confirmPassword)
+          .digest("hex");
+    
+        const securityUser: IUser = {
+          ...newUser,
+          password: hashPassword,
+          confirmPassword: hashPasswordConfirm
+        };
+    
+        return securityUser;
+    } catch(error: unknown) {
+        if (error instanceof SignUpError) {
+            throw new Error("Error to sign user up")
+        }
     }
+  }
 }
