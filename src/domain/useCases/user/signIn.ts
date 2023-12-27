99d3831ed5c0f3f0c4c358bef72a99e232291a87
Eduardo Carvalho <educarvalho009@gmail.com>
2023-12-27 12:41:04 -0400

    feat/test: signIn

---
+++ src/domain/useCases/user/signIn.ts
@@ -0,0 +1,21 @@
+import { CannotSignIn, SignInError } from "../../../providers/errors";
+import { UserRepository } from "../../../respository/user";
+
+export class SignInUserUseCase {
+    constructor(private repository: UserRepository) {}
+
+    async execute(email: string, password: string) {
+        try {
+            if(email === '' || password === '') {
+                throw new CannotSignIn("Email or/and Password cannot be empty");
+            }
+
+            const user = await this.repository.signIn(email, password);
+            return user;
+        } catch(error: unknown) {
+            if (error instanceof SignInError) {
+                throw new SignInError("Error to sign user in.")
+            }
+        }
+    }
+}
