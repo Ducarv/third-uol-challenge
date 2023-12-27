42ce60995c2865438bfdc2ccab7e2f7b0c9ea2b1
Eduardo Carvalho <educarvalho009@gmail.com>
2023-12-27 16:02:40 -0400

    refactor: auth middleware

--- src/providers/middlewares/auth.ts
+++ src/providers/middlewares/auth.ts
@@ -1,23 +1,29 @@
 import { NextFunction, Request, Response } from "express";
-import * as jwt from "jsonwebtoken"
-import "dotenv/config"
+import * as jwt from "jsonwebtoken";
+import "dotenv/config";
 
-export function authToken(request: Request, response: Response, next: NextFunction) {
-    const token = request.headers.authorization;
+export function authToken(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const token =
+    request.headers.authorization &&
+    request.headers.authorization.split(" ")[1];
 
-    if(!token) {
-        return response.status(401).json({ message: "Token not provided"})
+  if (!token) {
+    return response.status(401).json({ message: "Token not provided" });
+  }
+
+  const secret = process.env.SECRET_KEY as jwt.Secret;
+
+  jwt.verify(token, secret, (err, decoded) => {
+    if (err) {
+      return response.status(401).json({ message: " Token invalid" });
     }
 
-    const secret = process.env.SECRET_KEY as jwt.Secret;
+    request.body.user = decoded;
 
-    jwt.verify(token, secret, (err, decoded) => {
-        if(err) {
-            return response.status(401).json({ message: " Token invalid" });
-        }
-
-        request.body.user = decoded;
-
-        next();
-    })
+    next();
+  });
 }
