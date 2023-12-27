d34f20b192ea7fa5d25f10462a0e6a9a917ca47b
Eduardo Carvalho <educarvalho009@gmail.com>
2023-12-27 09:14:17 -0400

    test: signUp user

--- src/domain/useCases/user/__tests__/signUp.spec.ts
+++ src/domain/useCases/user/__tests__/signUp.spec.ts
@@ -1,5 +1,46 @@
+import { SignUpError } from "../../../../providers/errors";
+import { UserRepository } from "../../../../respository/user";
+import { IUser } from "../../../entities/User";
+import { SignUpUserUseCase } from "../signUp";
+
 describe("SignUp.ts", () => {
-    it("should sign a user up successfully", async () => {
-        
-    })
-})
+  let mockUser: IUser = {
+    firstName: "User",
+    lastName: "Mock",
+    birthDate: new Date(),
+    city: "Toronto",
+    country: "Canada",
+    email: "user@mail.com",
+    password: "123",
+    confirmPassword: "123",
+    events: [],
+  };
+  let mockRepository: Pick<UserRepository, "signUp"> = {
+    signUp: async (data: IUser) => ({ id: "1", ...data }),
+  };
+  let signUp = new SignUpUserUseCase(mockRepository as UserRepository);
+
+  it("should sign a user up successfully", async () => {
+    const sut = await signUp.execute(mockUser);
+    if (sut) {
+      expect(sut.id).toBeDefined();
+      expect(sut.firstName).toEqual("User");
+      expect(sut.email).toEqual("user@mail.com");
+    }
+  });
+
+  it("should handle errors correctly", async () => {
+    const error = new SignUpError("Error to sign user up");
+    jest.spyOn(signUp, "execute").mockRejectedValueOnce(error);
+
+    try {
+      //@ts-ignore
+      await signUp.execute();
+      fail("Throw Error");
+    } catch (caughtError: unknown) {
+      if (caughtError instanceof SignUpError) {
+        expect(caughtError.message).toEqual(error.message);
+      }
+    }
+  });
+});
