dbf28570af680701c0313b785aabec69ee2e5a6c
ducarv <educarvalho009@gmail.com>
2024-01-01 15:34:03 -0400

    test/refactor: get event by query

--- src/domain/useCases/event/__tests__/getByQuery.spec.ts
+++ src/domain/useCases/event/__tests__/getByQuery.spec.ts
@@ -19,25 +19,16 @@ describe('GetEventByQuery.ts', () => {
     },
   ];
 
-  const mockRepository: Pick<EventRepository, 'getEventByQuery'> = {
-    getEventByQuery: async (dayOfWeek?: string, desc?: string) => {
-      if (desc) {
-        return mockEvents.filter((event) =>
-          event.description.toLowerCase().includes(desc.toLowerCase()),
-        );
-      } else if (dayOfWeek) {
-        return mockEvents.filter(
-          (event) => event.dayOfWeek.toLowerCase() === dayOfWeek.toLowerCase(),
-        );
-      } else {
-        return mockEvents;
-      }
-    },
-  };
+  let getByQuery: GetEventByQueryUseCase;
+  let mockRepository: jest.Mocked<EventRepository>;
 
-  const getByQuery = new GetEventByQueryUseCase(
-    mockRepository as EventRepository,
-  );
+  beforeEach(() => {
+    mockRepository = {
+      getEventByQuery: jest.fn(),
+    } as unknown as jest.Mocked<EventRepository>;
+
+    getByQuery = new GetEventByQueryUseCase(mockRepository);
+  });
 
   it('should get events by day of the week correctly', async () => {
     const sut = await getByQuery.execute('friday');
@@ -64,15 +55,14 @@ describe('GetEventByQuery.ts', () => {
 
   it('should handle errors correctly', async () => {
     const error = new GetEventsError('Error to get events by query');
-    jest.spyOn(getByQuery, 'execute').mockRejectedValueOnce(error);
+    mockRepository.getEventByQuery.mockRejectedValueOnce(error);
 
     try {
       await getByQuery.execute();
       fail('Throw error');
     } catch (caughtError: unknown) {
-      if (caughtError instanceof GetEventsError) {
-        expect(caughtError.message).toEqual(error.message);
-      }
+      expect(caughtError).toBeInstanceOf(GetEventsError);
+      expect(caughtError).toEqual(error);
     }
   });
 });
