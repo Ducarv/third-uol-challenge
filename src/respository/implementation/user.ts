f9a1cb30292d6c1b0092703e9e8d92fe315bdff7
ducarv <educarvalho009@gmail.com>
2024-01-01 15:01:30 -0400

    refactor: user repository error handling

--- src/respository/implementation/user.ts
+++ src/respository/implementation/user.ts
@@ -7,28 +7,40 @@ export class UserPrismaRepository implements UserRepository {
   constructor() {}
 
   async signUp(data: IUser) {
-    const existUser = await prisma.user.findUnique({
-      where: {
-        email: data.email,
-      },
-    });
+    try {
+      const existUser = await prisma.user.findUnique({
+        where: {
+          email: data.email,
+        },
+      });
 
-    if (existUser) {
-      throw new SignUpError('Email already in use');
+      if (existUser) {
+        throw new SignUpError('Email already in use');
+      } else {
+        const newUser = await prisma.user.create({
+          data: {
+            ...(data as Omit<IUser, 'events'>),
+          },
+        });
+
+        return newUser as Partial<IUser>;
+      }
+    } catch (error: unknown) {
+      if (error instanceof SignUpError) {
+        throw new SignUpError('Email already in use');
+      }
     }
-
-    const newUser = await prisma.user.create({
-      data: {
-        ...(data as Omit<IUser, 'events'>),
-      },
-    });
-
-    return newUser as Partial<IUser>;
   }
 
   async signIn(email: string) {
-    const user = await prisma.user.findUnique({ where: { email } });
-    const result = user as Pick<IUser, 'firstName' | 'lastName' | 'email'>;
-    return result;
+    try {
+      const user = await prisma.user.findUnique({ where: { email } });
+      const result = user as Pick<IUser, 'firstName' | 'lastName' | 'email'>;
+      return result;
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        throw new Error(error.message);
+      }
+    }
   }
 }
--- src/respository/user.ts
+++ src/respository/user.ts
@@ -1,6 +1,6 @@
 import { IUser } from '../domain/entities/User';
 
 export interface UserRepository {
-  signUp: (data: IUser) => Promise<Partial<IUser>>;
-  signIn: (email: string) => Promise<Partial<IUser>>;
+  signUp: (data: IUser) => Promise<Partial<IUser | undefined>>;
+  signIn: (email: string) => Promise<Partial<IUser | undefined>>;
 }
